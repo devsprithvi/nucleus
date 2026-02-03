7f67ccbbcdbd9970e30fdc1ba995fd685af2311c
Prudhvi <devs.prudhvi@gmail.com>
2026-02-03 05:54:59 +0530

    fix(terraform): remove sleep_time arg from hf space module

--- terraform/modules/huggingface/spaces/main.tf
+++ terraform/modules/huggingface/spaces/main.tf
@@ -1,10 +1,9 @@
 resource "huggingface-spaces_space" "this" {
-  name       = var.name
-  private    = var.private
-  sdk        = var.sdk
-  template   = var.template
-  hardware   = var.hardware
-  sleep_time = var.sleep_time
-  secrets    = var.secrets
-  variables  = var.variables
+  name      = var.name
+  private   = var.private
+  sdk       = var.sdk
+  template  = var.template
+  hardware  = var.hardware
+  secrets   = var.secrets
+  variables = var.variables
 }
